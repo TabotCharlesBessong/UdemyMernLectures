c37a5042a093ff1a8ae899c4eaa9e81b2ebf7f2b
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2021-12-28 08:37:53 +0100

    learning express basics

--- 2.Express/server.js
+++ 2.Express/server.js
@@ -12,3 +12,29 @@ app.listen(port,()=>{
 app.get('/',(req,res)=>{
   res.send("<br><br><br><br><center><h1>Server is <hr> up and running</h1></center>")
 })
+
+app.get('/about',(req,res)=>{
+  res.send("<center><br><br><br><br><h1><b><i>My email adress</i></b></h1></center>")
+}) 
+
+app.get('/charles',(req,res)=>{
+  res.send(`
+  <center>
+  <h1>
+    <b>
+    Hello my i am called Charles and i am software developer i 
+program in  </b>
+  </h1>
+  <ul>
+    <li>Html</li>
+    <li>css</li>
+    <li>js</li>
+    <li>c++</li>
+    <li>c</li>
+    <li>Java</li>
+    <li>Python</li>
+    <li>React</li>
+  </ul>
+</center>
+  `)
+})
