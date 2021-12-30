d12d6a871542fa0866d80bda843d9fbeaf585e50
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2021-12-30 01:23:13 +0100

    Done with the rest Api

--- 4.RestApi/server.js
+++ 4.RestApi/server.js
@@ -29,7 +29,7 @@ app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
 })
 
-app.get('/',(req,res)=>{
+app.get('/barcelonas',(req,res)=>{
   Barca.find((error,barcelonas)=>{
     if(barcelonas){
       console.log(barcelonas);
@@ -44,4 +44,49 @@ app.get('/',(req,res)=>{
   })
   // res.write("<h1>Hello dude , thank you for joining us</h1>")
   // res.send()
+})
+
+
+app.post("/barcelonas",(req,res)=>{
+
+  const newBarca = new Barca({
+    name:req.body.name,
+    desc:req.body.desc
+  })
+  newBarca.save((err)=>{
+    if(!err){
+      console.log(`The article was successfully added`);
+    }
+  })
+
+  console.log(req.body.name);
+  console.log(req.body.desc);
+})
+
+app.delete("/barcelonas",(req,res)=>{
+  Barca.deleteMany((err)=>{
+    if(!err){
+      console.log("all the articles have been deleted");
+    }
+  })
+})
+
+app.get("/barcelonas/:playerInfo",(req,res)=>{
+  Barca.findOne({name:req.params.playerInfo},(err,barka)=>{
+    if(!err){
+      res.send(barka)
+      console.log(barka);
+    }else{
+      console.log(err);
+    }
+    
+  })
+})
+
+app.delete("/barcelonas/:playerInfo",(req,res)=>{
+  Barca.deleteOne({name:req.params.playerInfo},(err)=>{
+    if(!err){
+      console.log("The article was deleted");
+    }
+  })
 })
