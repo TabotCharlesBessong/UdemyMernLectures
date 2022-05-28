4a28aa7eb82cb0bcc19b3215b891e00005ec547d
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-28 23:48:34 +0100

    added storage

--- 12.Startingprojects/src/redux/root-reducer.js
+++ 12.Startingprojects/src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 
 import { useReducer } from "react";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import cartReducer from "./cart/CartReducers";
 import userReducer from "./user/userReducer";
 
-export default combineReducers({
+const persistConfig = {
+  key:'root',
+  storage,
+  whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
   user:userReducer ,
   cart:cartReducer
 })
+
+export default persistReducer(persistConfig,rootReducer)
