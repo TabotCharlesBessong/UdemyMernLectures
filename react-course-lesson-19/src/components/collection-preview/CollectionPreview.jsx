fc34f80a20d622e9483a37a53fa657d3e0966663
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-06-27 22:34:10 +0100

    i hope this solves my problens but something is missing

---
+++ react-course-lesson-19/src/components/collection-preview/CollectionPreview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/CollectionItem';
+
+import './collectionPreview.scss';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
