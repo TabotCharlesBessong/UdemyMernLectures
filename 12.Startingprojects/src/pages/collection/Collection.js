8ad493335a71482661c2fe9b45ddff2c95f2e160
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-07-02 17:47:22 +0100

    some changes

--- 12.Startingprojects/src/pages/collection/Collection.js
+++ 12.Startingprojects/src/pages/collection/Collection.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CardItem from '../../components/cardItem/CardItem';
 
-import { selectcollection } from '../../redux/shop/shopSelector';
+import { selectCollection } from '../../redux/shop/shopSelector';
 
 import './Collection.scss';
 
 const Collection = ({ collection }) => {
-  const { title, items } = collection;
+  console.log(collection)
+  // const { title, items } = collection;
   return (
     <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
+      <h2 className='title'></h2>
       <div className='items'>
-        {items.map(item => (
+        {/* {items.map(item => ( */}
           <CardItem key={item.id} item={item} />
-        ))}
+        {/* ))} */}
       </div>
     </div>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectcollection(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(Collection);
